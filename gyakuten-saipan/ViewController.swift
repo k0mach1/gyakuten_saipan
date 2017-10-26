877b00d48b4c1471f253bcf4bddeecf0c300a0b9
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-10-27 08:32:24 +0900

    Initial Commit

---
+++ gyakuten-saipan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gyakuten-saipan
+//
+//  Created by 町田公佑 on 2017/10/27.
+//  Copyright © 2017年 k0mach1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gyakuten-saipanTests/gyakuten_saipanTests.swift
@@ -0,0 +1,36 @@
+//
+//  gyakuten_saipanTests.swift
+//  gyakuten-saipanTests
+//
+//  Created by 町田公佑 on 2017/10/27.
+//  Copyright © 2017年 k0mach1. All rights reserved.
+//
+
+import XCTest
+@testable import gyakuten_saipan
+
+class gyakuten_saipanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
